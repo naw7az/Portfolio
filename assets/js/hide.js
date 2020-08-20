7d0453c689a15af601a17a784ccd40803aa54235
naw7az <nawazadil17@outlook.com>
2020-08-20 07:11:19 +0530

    JS and CSS files

---
+++ assets/js/hide.js
@@ -0,0 +1,19 @@
+function openNav(id) {
+    document.getElementById(id).style.height = "95%";
+    document.getElementById('main').style.filter = "blur(4px)";
+    document.getElementById('header').style.filter = "blur(4px)";
+    document.getElementById('main').style.pointerEvents = "none";
+    document.getElementById('header').style.pointerEvents = "none";
+    document.documentElement.style.overflow = 'hidden';
+    document.body.scroll = "no";
+}
+
+function closeNav(id) {
+    document.getElementById(id).style.height = "0%";
+    document.getElementById('main').style.filter = 'none';
+    document.getElementById('header').style.filter = 'none';
+    document.getElementById('main').style.pointerEvents = "auto";
+    document.getElementById('header').style.pointerEvents = "auto";
+    document.documentElement.style.overflowY = 'scroll';
+    document.body.scroll = "yes";
+}
