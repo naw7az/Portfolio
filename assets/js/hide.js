c8d56f5fbb7cc6e3e2f2829b59b3b4792bf2961e
naw7az <nawazadil17@outlook.com>
2020-08-23 17:45:23 +0530

    navbar hiding for mobile

--- assets/js/hide.js
+++ assets/js/hide.js
@@ -4,6 +4,7 @@ function openNav(id) {
     document.getElementById('header').style.filter = "blur(4px)";
     document.getElementById('main').style.pointerEvents = "none";
     document.getElementById('header').style.pointerEvents = "none";
+    document.getElementById('headerToggle').style.display = "none";
     document.documentElement.style.overflow = 'hidden';
     document.body.scroll = "no";
 }
@@ -14,6 +15,7 @@ function closeNav(id) {
     document.getElementById('header').style.filter = 'none';
     document.getElementById('main').style.pointerEvents = "auto";
     document.getElementById('header').style.pointerEvents = "auto";
+    document.getElementById('headerToggle').style.display = "block";
     document.documentElement.style.overflowY = 'scroll';
     document.body.scroll = "yes";
 }
