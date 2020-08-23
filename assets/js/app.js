d4d7a87a16a49ec7aead188933cbe23af6f051a6
naw7az <nawazadil17@outlook.com>
2020-08-23 16:40:36 +0530

    making firebase database

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,67 @@
-var submit = document.getElementById("sub");
-var inputName = document.getElementById("username");
-var inputMail = document.getElementById("usermail");
-var inputMessage = document.getElementById("usermessage");
+var firebaseConfig = {
+    apiKey: "AIzaSyDi5cCrhZ4rUcPGvTbGE0rn2U3KzMmEp5Q",
+    authDomain: "portfolio-an.firebaseapp.com",
+    databaseURL: "https://portfolio-an.firebaseio.com",
+    projectId: "portfolio-an",
+    storageBucket: "portfolio-an.appspot.com",
+    messagingSenderId: "991049660178",
+    appId: "1:991049660178:web:b558e79399c8ddc5ee65cb",
+    measurementId: "G-JYZHG2PV83"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
-function sendToConsole() {
-    if (inputName.value.length > 0 && inputName.value.length > 0 && inputMessage.value.length > 0) {
-        console.log(inputName.value);
-        console.log(inputMail.value);
-        console.log(inputMessage.value);
+// Reference message collection
+var messageRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById("sub").addEventListener('click', submitForm);
+
+// Submit Form
+
+function submitForm() {
+    // Get values
+    var inputName = document.getElementById("username");
+    var inputMail = document.getElementById("usermail");
+    var inputMessage = document.getElementById("usermessage");
+
+    // Save Message
+    if (inputName.value.length > 0 && inputMail.value.length > 0 && inputMessage.value.length > 0) {
+        saveMessage(inputName.value, inputMail.value, inputMessage.value);
         inputName.value = "";
         inputMail.value = "";
         inputMessage.value = "";
+        alert("Your message has been sent.");
     }
 }
 
-submit.addEventListener("click", sendToConsole);
+function saveMessage(name, email, message) {
+    var newMessageRef = messageRef.push();
+    newMessageRef.set({
+        name: name,
+        email: email,
+        message: message
+    });
+}
+
+
+
+
+// var submit = document.getElementById("sub");
+// var inputName = document.getElementById("username");
+// var inputMail = document.getElementById("usermail");
+// var inputMessage = document.getElementById("usermessage");
+
+// function sendToConsole() {
+//     if (inputName.value.length > 0 && inputName.value.length > 0 && inputMessage.value.length > 0) {
+//         console.log(inputName.value);
+//         console.log(inputMail.value);
+//         console.log(inputMessage.value);
+//         inputName.value = "";
+//         inputMail.value = "";
+//         inputMessage.value = "";
+//     }
+// }
+
+// submit.addEventListener("click", sendToConsole);
